40bf995818a7010ac6ecca9369ece9ac565358fd
jaketrent <trent.jake@gmail.com>
2015-08-03 07:58:30 -0600

    Make changeset error use jsonapi format

--- web/views/changeset_view.ex
+++ web/views/changeset_view.ex
@@ -2,8 +2,21 @@ defmodule DemoPhoenixOauth.ChangesetView do
   use DemoPhoenixOauth.Web, :view
 
   def render("error.json", %{changeset: changeset}) do
-    # When encoded, the changeset returns its errors
-    # as a JSON object. So we just pass it forward.
-    %{errors: changeset}
+    %{errors: Enum.map(changeset.errors, &fmt_validation_error/1)}
+  end
+
+  defp fmt_validation_error({ key, err }) do
+    %{ detail: fmt_detail(key, err),
+       source: fmt_pointer(key),
+       status: 422 }
+  end
+
+  defp fmt_detail(key, err) do
+   String.capitalize(Atom.to_string(key)) <> " " <> err
+  end
+
+  defp fmt_pointer(key) do
+    %{ pointer: "/data" <> "/" <> Atom.to_string(key) }
   end
 end
+
