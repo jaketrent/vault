12455949e638936605900391f514d7c616d9706c
jaketrent <trent.jake@gmail.com>
2015-08-03 07:57:01 -0600

    Make general error use jsonapi format

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,17 +1,33 @@
 defmodule DemoPhoenixOauth.ErrorView do
   use DemoPhoenixOauth.Web, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
+  def render("404.json", assigns) do
+    %{errors: [render_one(assigns.reason, QuotesApi.ErrorView, "error.json") ]}
   end
 
-  def render("500.html", _assigns) do
-    "Server internal error"
+  def render("500.json", assigns) do
+    %{errors: [render_one(assigns.reason, QuotesApi.ErrorView, "error.json") ]}
+  end
+
+  def render("error.json", reason) do
+    status = Map.fetch(reason.error, :plug_status)
+    %{ code: get_code(status),
+       detail: (if Map.has_key?(reason.error, :message), do: String.capitalize(reason.error.message), else: "Generic Error"),
+       status: status }
   end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
-    render "500.html", assigns
+    render "500.json", assigns
+  end
+
+  defp get_code(plug_status) do
+    case plug_status do
+      400 -> "bad-request"
+      404 -> "not-found"
+      422 -> "validation"
+      _ -> "internal-server"
+    end
   end
 end
