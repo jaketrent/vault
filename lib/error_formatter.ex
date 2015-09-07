3bf3e844d523ca4ac5de86c216067517c18df5f7
jaketrent <trent.jake@gmail.com>
2015-09-07 17:17:12 -0600

    Handle changesets when field keys are underscored

--- lib/error_formatter.ex
+++ lib/error_formatter.ex
@@ -1,3 +1,5 @@
+import Phoenix.Naming, only: [camelize: 1, humanize: 1]
+
 defmodule ErrorFormatter do
   def get_msg(error) do
     case Map.fetch(error, :message) do
@@ -14,7 +16,7 @@ defmodule ErrorFormatter do
   end
 
   def fmt_detail(key, msg) do
-    (Atom.to_string(key) <> " " <> msg)
+    (humanize(Atom.to_string(key)) <> " " <> msg)
     |> fmt_detail
   end
 
@@ -23,6 +25,11 @@ defmodule ErrorFormatter do
   end
 
   def fmt_pointer(key) do
-    %{ pointer: "/data" <> "/" <> Atom.to_string(key) }
+    %{ pointer: "/data" <> "/" <> fmt_key(key) }
+  end
+
+  defp fmt_key(key) do
+    camel = camelize(Atom.to_string(key))
+    String.downcase(String.slice(camel, 0, 1)) <> String.slice(camel, 1..-1)
   end
 end
